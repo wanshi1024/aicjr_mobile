e9ae090edc33ff21638b9bb57a54bd97f22a7611
Your Namecls <you@example.com>
2019-07-31 15:07:45 +0800

    ws

---
+++ js/register.js
@@ -0,0 +1,75 @@
+
+
+// 企业注册事件
+$(document).on("pagecreate", "#enterprise", function () {
+    let page = $("#enterprise");
+
+    // 获取验证码
+    page.find(".get-captcha").on("tap", function (e) {
+        // 第一个参数 电话输入框的jq对象 第二个参数 获取验证码的按钮jq对象
+        getCaptcha(page.find("input.tel"), $(this));
+
+    });
+
+    // 确认注册 
+    page.find(".confirm-register").on("tap", function (e) {
+        // 验证码转小写传入
+        confirmRegister(page.find("input.captcha").val().toLowerCase());
+    });
+
+});
+
+
+/**
+ * 获取验证码倒计时
+ * @param {*} telInput 电话输入框的jq对象
+ * @param {*} elem 获取验证码的按钮jq对象
+ */
+function getCaptcha(telInput, elem) {
+    let phoneNum = telInput.val();
+    if (isAvailablePhone(phoneNum)) {
+        telInput.attr("disabled", "disabled"); //验证码倒计时开始 设置电话输入框禁用
+        let t = 5;
+        t--;
+        elem.text(`${t}s后重新获取`);
+        let timer = setInterval(() => {
+            if (t === 0) {
+                clearInterval(timer);
+                elem.text("获取验证码");
+                telInput.removeAttr("disabled"); // 恢复电话输入框
+            } else {
+                t--;
+                elem.text(`${t}s后重新获取`);
+            }
+        }, 1000);
+    } else {
+        alert(`请正确输入手机号码`)
+    }
+
+}
+
+/**
+ * 判断手机号是否为11位有效号码
+ * @param {String} phoneNum 手机号
+ */
+function isAvailablePhone(phoneNum) {
+    var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    if (!myreg.test(phoneNum)) {
+        return false; //号码不合法
+    } else {
+        return true; //号码符合 
+    }
+}
+
+/**
+ * 确认注册
+ * @param {String} captcha 验证码字符串
+ */
+function confirmRegister(captcha) {
+    if (captcha === '' || captcha.length != 4) {
+        alert(`请正确输入验证码`);
+    } else {
+        location.href = "./demo.html";
+    }
+
+}
